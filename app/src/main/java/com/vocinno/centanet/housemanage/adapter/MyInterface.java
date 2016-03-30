ce768bea0bc0dbe5c6fca05b0b5be7f24fae0de6
zhongrui <2380253499@qq.com>
2016-03-30 17:17:53 +0800

    拍照功能。

---
+++ app/src/main/java/com/vocinno/centanet/housemanage/adapter/MyInterface.java
@@ -0,0 +1,6 @@
+package com.vocinno.centanet.housemanage.adapter;
+
+public interface MyInterface {
+    void takePhoto(String type);
+    void editPhoto(String type,String imgPath,String describe);
+}

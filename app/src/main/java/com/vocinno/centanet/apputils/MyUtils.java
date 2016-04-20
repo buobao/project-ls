e18a153e32e134e18e717a541e2b4691f9c9afb3
zhongrui <2380253499@qq.com>
2016-04-20 16:45:16 +0800

    自定义工具类

--- app/src/main/java/com/vocinno/centanet/apputils/MyUtils.java
+++ app/src/main/java/com/vocinno/centanet/apputils/MyUtils.java
@@ -1,11 +1,17 @@
 package com.vocinno.centanet.apputils;
 
+import android.app.Activity;
 import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/4/13.
  */
 public  class MyUtils {
+    public static List<Activity> actList;
     public static int px2dip(Context context, float pxValue){
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int)(pxValue / scale + 0.5f);
@@ -13,5 +19,23 @@ public  class MyUtils {
     public static int dip2px(Context context, float dipValue){
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int)(dipValue * scale + 0.5f);
+    }
+    public static void addActivityToList(Activity activity){
+        if(actList==null){
+            actList=new ArrayList<Activity>();
+        }
+        actList.add(activity);
+    }
+    public static void removeActivityFromList(){
+        if(actList!=null){
+            /*for (int i = 0; i <actList.size() ; i++) {
+                actList.get(i).finish();
+            }*/
+            Iterator it = actList.iterator();
+            while (it.hasNext()) {
+                it.next();
+                ((Activity)it.next()).finish();
+            }
+        }
     }
 }

242b8e8d1d1cd86a6e87b9307a74e969f035daf2
zhongrui <2380253499@qq.com>
2016-04-20 11:24:54 +0800

    房源数据列表自定义获取数据接口

---
+++ app/src/main/java/com/vocinno/centanet/myinterface/GetDataInterface.java
@@ -0,0 +1,8 @@
+package com.vocinno.centanet.myinterface;
+
+/**
+ * Created by Administrator on 2016/4/20.
+ */
+public interface GetDataInterface {
+    void getListData(String  type,String price,String square,String frame,String tag,String usageType,int page,int pageSize,String sidx,String sord,String searchId,String searchType);
+}

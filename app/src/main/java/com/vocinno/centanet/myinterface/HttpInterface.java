59f5bd348821a61b5b0b661b2d1d58efbd7ac31f
zhongrui <2380253499@qq.com>
2016-06-02 18:41:58 +0800

    删除冲突文件

--- app/src/main/java/com/vocinno/centanet/myinterface/HttpInterface.java
+++
@@ -1,8 +0,0 @@
-package com.vocinno.centanet.myinterface;
-
-/**
- * Created by Administrator on 2016/4/18.
- */
-public interface HttpInterface {
-    public void netWorkResult(String name, String className, Object data);
-}
--- app/src/main/java/com/vocinno/centanet/myinterface/TagSlidingInterface.java
+++
@@ -1,8 +0,0 @@
-package com.vocinno.centanet.myinterface;
-
-/**
- * Created by Administrator on 2016/5/18.
- */
-public interface TagSlidingInterface {
-    void sliding(boolean flag);
-}

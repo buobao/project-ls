32bd23d8c4ebd1f55b401ec2577c4a55048883e0
fanxl2 <fanxl2@centaline.com.cn>
2016-06-20 16:04:37 +0800

    修复手机号码验证规则问题;

--- app/src/main/java/com/vocinno/centanet/customermanage/AddCustomerActivity.java
+++ app/src/main/java/com/vocinno/centanet/customermanage/AddCustomerActivity.java
@@ -960,7 +960,7 @@ public class AddCustomerActivity extends OtherBaseActivity implements View.OnTou
 
     public static boolean isMobileNO(String mobiles) {
         if (mobiles != null) {
-            Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9])|(17[0,7]))\\d{8}$");
+            Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0-9])|(17[0-9]))\\d{8}$");
             Matcher m = p.matcher(mobiles);
             return m.matches();
         }

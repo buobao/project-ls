09706a624cebadebc65547eabcde79a80ead861f
hewei26 <hewei26@centaline.com.cn>
2016-06-20 14:54:38 +0800

    选择陪看人

--- app/src/main/java/com/vocinno/centanet/tools/constant/MyConstant.java
+++ app/src/main/java/com/vocinno/centanet/tools/constant/MyConstant.java
@@ -54,6 +54,8 @@ public class MyConstant {
     /*************************添加二手**************************/
     public static final String addSecondHouse ="addSecondHouse";
 
+    //一手&二手选择陪看人
+    public static final int REQUEST_CHOOSE_PEOPLE = 301;
 
 
 
